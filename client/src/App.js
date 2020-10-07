6d77ce8ac248499959e35d319500d06906707a80
kelechiodom10 <kelechiodom10@gmail.com>
2020-10-07 12:54:46 +0100

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-	return <div className="App">Hi World</div>;
+	return <div className="App">Hello World</div>;
 }
 
 export default App;
