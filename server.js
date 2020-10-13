7c298ae17f96ab7a06a730ef8faebeb395989238
kelechiodom10 <kelechiodom10@gmail.com>
2020-10-13 15:46:59 +0100

    Edited the file structure of the server.js file to make use of the routes

--- server.js
+++ server.js
@@ -4,94 +4,21 @@ const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
 
-mongoose.connect("mongodb://localhost/simple-demo", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+connectDB();
 
-const {
-	getTodos,
-	createTodo,
-	findTodoById,
-	updateTodoById,
-	deleteTodoById,
-} = require("./controllers/todoController");
+const todos = require("./routes/todos");
 
 const app = express();
 const port = process.env.PORT || 5005;
 
 app.use(cors());
 app.use(express.json());
-//parses request body and populates request.body
-// app.use(express.bodyParser());
-//checks request.body for HTTP method overrides
-// app.use(express.methodOverride());
 
-app.get("/", (req, res) => {
-	res.status(200);
-	res.json("did this work");
-});
-
-app.get("/todos", async (req, res) => {
-	res.status(202);
-	const todos = await getTodos();
-	res.json({
-		status: "success",
-		data: {
-			todos,
-		},
-	});
-});
-
-app.get("/todos/:id", async (req, res) => {
-	res.status(202);
-	const todo = await findTodoById(req.params.id);
-	const todoReturned = todo.toObject();
-	res.json({
-		status: "success",
-		data: {
-			todo: { id: todo.id, ...todoReturned },
-		},
-	});
-});
-
-app.post("/todos", async (req, res) => {
-	res.status(201);
-	const { text, title } = req.body;
-	const todo = await createTodo({ text, title });
-	const todoReturned = todo.toObject();
-	res.json({
-		status: "success",
-		data: {
-			todo: { id: todo.id, ...todoReturned },
-		},
-	});
-});
-
-app.put("/todos/:id", async (req, res) => {
-	res.status(202);
-	const todo = await updateTodoById(req.params.id, req.body);
-	const todoReturned = todo.toObject();
-	res.json({
-		status: "success",
-		data: {
-			todo: { id: todo.id, ...todoReturned },
-		},
-	});
-});
-
-app.delete("/todos/:id", async (req, res) => {
-	res.status(204);
-	await deleteTodoById(req.params.id);
-	res.json({
-		status: "success",
-		data: null,
-		message: "Todo has been deleted successfully",
-	});
-});
+app.use("/api/v1/todos", todos);
 
 app.listen(port, () => {
 	console.log(
