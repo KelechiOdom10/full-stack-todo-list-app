5e5fa950c96114429792f43c3278d549f577faa6
kelechiodom10 <kelechiodom10@gmail.com>
2020-10-19 14:56:35 +0100

    Added user todo model to reference the specific user that created that Todo via the user's id

--- models/todoModel.js
+++ models/todoModel.js
@@ -4,6 +4,7 @@ const todoSchema = new mongoose.Schema({
 	title: { type: String, required: [true, "Please add a Title for Todo"] },
 	text: { type: String, required: [true, "Please add a description for Todo"] },
 	completed: { type: Boolean, default: false },
+	user: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
