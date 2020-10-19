35906e174c4cade1da1122299d9cbcf0a26ce634
kelechiodom10 <kelechiodom10@gmail.com>
2020-10-19 15:01:25 +0100

    Mounted auth router in server.js file and added errorHandler as well as cookie-parser

--- server.js
+++ server.js
@@ -4,25 +4,40 @@ const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
+//? Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//? Connect to DB
 connectDB();
 
+//? Route Files
 const todos = require("./routes/todos");
+const auth = require("./routes/auth");
 
 const app = express();
 const port = process.env.PORT || 5005;
 
 app.use(cors());
+//? Body Parser
 app.use(express.json());
+//? Cookie Parser
+app.use(cookieParser());
 
+//? Dev logging Middleware
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
 
+//? Mount routers
 app.use("/api/v1/todos", todos);
+app.use("/api/v1/auth", auth);
+
+//? Use error Handler
+app.use(errorHandler);
 
 app.listen(port, () => {
 	console.log(
