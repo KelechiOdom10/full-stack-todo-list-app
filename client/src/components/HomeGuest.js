b1545d998dbe658c8cef6a258ded6109d9f5cff0
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-22 23:10:33 +0000

    Design Changes and Context added

--- client/src/components/HomeGuest.js
+++ client/src/components/HomeGuest.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useGlobalState } from "../context/GlobalState";
+import React from "react";
 import AddTodos from "./AddTodos";
 import TodoList from "./TodoList";
 
